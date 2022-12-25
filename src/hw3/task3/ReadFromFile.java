e9436b4c2303f1ac648c1da58a9166ebf780e881
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2022-12-25 22:04:44 +0200

    Initial commit

---
+++ src/hw3/task3/ReadFromFile.java
@@ -0,0 +1,19 @@
+package hw3.task3;
+
+import java.io.*;
+
+class ReadFromFile {
+    public static void main(String[] args) {
+
+        try (BufferedReader bufferedReader = new BufferedReader
+                (new InputStreamReader(new FileInputStream("G:\\_java cbs\\homeWork\\JavaAdvanced\\src\\hw3\\task3\\text.txt"), "Cp866"))) {
+            String string;
+            while ((string = bufferedReader.readLine()) != null) {
+                System.out.println(string);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
