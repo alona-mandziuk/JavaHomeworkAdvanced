de9989ef2feb36b578ef1ec26d0c9737a8c1127c
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-01-10 22:26:59 +0200

    Initial commit

---
+++ src/hw8/task3/Sweets.java
@@ -0,0 +1,68 @@
+package hw8.task3;
+
+class Sweets {
+    String sweet;
+    boolean onPlate = true;
+
+    Sweets(String sweet) {
+        this.sweet = sweet;
+    }
+
+    synchronized void eat(Kids kid) throws InterruptedException {
+        if (onPlate) {
+            Thread.sleep(100);
+            onPlate = false;
+            System.out.println("Kid " + kid.kidName + " ate " + this.sweet);
+        }
+    }
+}
+
+class Kids extends Thread {
+    String kidName;
+    Sweets[] sweets;
+
+    Kids(String kidName, Sweets[] sweets) {
+        this.kidName = kidName;
+        this.sweets = sweets;
+    }
+
+    @Override
+    public synchronized void run() {
+        for (int i = 0; i < sweets.length; i++) {
+            if (sweets[i].onPlate) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                sweets[i].onPlate = false;
+                System.out.println("Kid " + kidName + " ate " + sweets[i].sweet);
+
+            }
+            try {
+                sweets[i].eat(this);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class EatingCandies {
+    public static void main(String[] args) {
+        Sweets[] sweets = {new Sweets("Cookie"),
+                new Sweets("Candie"), new Sweets("Chocolate"),
+                new Sweets("Cake"), new Sweets("Waffles")};
+        Kids kid1 = new Kids("Bob", sweets);
+        Kids kid2 = new Kids("Mark", sweets);
+        Kids kid3 = new Kids("Andrew", sweets);
+        Kids kid4 = new Kids("Ammy", sweets);
+        Kids kid5 = new Kids("Katy", sweets);
+
+        kid1.start();
+        kid2.start();
+        kid3.start();
+        kid4.start();
+        kid5.start();
+    }
+}
