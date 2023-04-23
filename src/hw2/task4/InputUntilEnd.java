94ccb82e0f902e2e96cbbd9ad890f623c2fda19f
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-04-23 22:30:07 +0300

    Initial commit

--- src/hw2/task4/InputUntilEnd.java
+++ src/hw2/task4/InputUntilEnd.java
@@ -1,9 +1,16 @@
 package hw2.task4;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 class InputUntilEnd {
+    static void shopList(List<String> list) {
+        for (String s : list) {
+            System.out.println(" - " + s);
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList<String> shoppingList = new LinkedList<>();
         System.out.println("--------------------------------------------");
@@ -19,9 +26,7 @@ class InputUntilEnd {
         shoppingList.removeLast();
         System.out.println("-------------------------------------------------");
         System.out.println("Your shopping list: ");
-        for (String s : shoppingList) {
-            System.out.println(" - " + s);
-        }
+        shopList(shoppingList);
         System.out.println("-------------------------------------------------");
     }
 }
