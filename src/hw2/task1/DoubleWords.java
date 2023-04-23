bc67d046d3fede3fac8ceeba4f5a02aca2b4f320
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-04-23 22:04:19 +0300

    Initial commit

--- src/hw2/task1/DoubleWords.java
+++ src/hw2/task1/DoubleWords.java
@@ -1,44 +1,50 @@
 package hw2.task1;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class DoubleWords {
-    static void doubleValues(ArrayList<String> arrayLists) {
-        for (int i = 0; i < arrayLists.size(); i++) {
-            System.out.println(arrayLists.get(i) + ", " + arrayLists.get(i));
+    static void doubleValues(List list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i) + ", " + list.get(i));
         }
-
     }
 
+    static void showList(List list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            System.out.print(list.get(i) + ", ");
+        }
+        System.out.print(list.get(list.size() - 1));
+    }
+
+
     public static void main(String[] args) {
-        ArrayList<String> words = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("---------------------------------------------");
+        System.out.println("--------------------------------------------");
         System.out.println("Input the 1st word: ");
         String word1 = scanner.next();
-        words.add(word1);
         System.out.println("Input the 2nd word: ");
         String word2 = scanner.next();
-        words.add(word2);
         System.out.println("Input the 3d word: ");
         String word3 = scanner.next();
-        words.add(word3);
         System.out.println("Input the 4th word: ");
         String word4 = scanner.next();
-        words.add(word4);
         System.out.println("Input the 5th word: ");
         String word5 = scanner.next();
-        words.add(word5);
-        System.out.println("---------------------------------------------");
+
+        List<String> words = List.of(word1, word2, word3, word4, word5);
+        System.out.println("--------------------------------------------");
         System.out.println("Array before deformation: ");
-        for (String w : words) {
-            System.out.print(w + ", ");
-        }
+
+        showList(words);
+
         System.out.println();
-        System.out.println("---------------------------------------------");
+        System.out.println("--------------------------------------------");
         System.out.println("Array after deformation: ");
+
         doubleValues(words);
-        System.out.println("---------------------------------------------");
+
+        System.out.println("--------------------------------------------");
     }
 }
