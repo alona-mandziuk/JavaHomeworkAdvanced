0b804b5c2e93f7e1f967313abc54d5cccb54c28e
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-05-14 20:28:12 +0300

    Initial commit

--- src/hw4/task3/DaemonThread.java
+++ src/hw4/task3/DaemonThread.java
@@ -8,33 +8,30 @@ class DaemonThread extends Thread {
            System.out.println("It`s user thread.");
        }
     }
+    static void daemonThreadInfo (Thread thread){
+        System.out.println("---------------------------");
+        System.out.println("Get Name: " + thread.getName());
+        System.out.println("Is Daemon? " + thread.isDaemon());
+        System.out.println("Get id: " + thread.getId());
+        System.out.println("Get priority: " + thread.getPriority());
+        System.out.println("Get thread group: " + thread.getThreadGroup());
+        System.out.println("Is alive: " + thread.isAlive());
+        System.out.println("Get class: " + thread.getClass());
+        System.out.println("---------------------------");
+    }
 
     public static void main(String[] args) {
-        DaemonThread thread1 = new DaemonThread();
-        DaemonThread thread2 = new DaemonThread();
+        Thread thread1 = new DaemonThread();
+        Thread thread2 = new DaemonThread();
         thread1.setDaemon(true);
         thread2.setDaemon(false);
         System.out.println("---------------------------");
         System.out.println("The first thread: ");
         thread1.start();
-        System.out.println("Get Name: " + thread1.getName());
-        System.out.println("Is Daemon? " + thread1.isDaemon());
-        System.out.println("Get id: " + thread1.getId());
-        System.out.println("Get priority: " + thread1.getPriority());
-        System.out.println("Get thread group: " + thread1.getThreadGroup());
-        System.out.println("Is alive: " + thread1.isAlive());
-        System.out.println("Get class: " + thread1.getClass());
-        System.out.println("---------------------------");
+        daemonThreadInfo(thread1);
         System.out.println("The second thread: ");
         thread2.start();
-        System.out.println("Get Name: " + thread2.getName());
-        System.out.println("Is Daemon? " + thread2.isDaemon());
-        System.out.println("Get id: " + thread2.getId());
-        System.out.println("Get priority: " + thread2.getPriority());
-        System.out.println("Get thread group: " + thread2.getThreadGroup());
-        System.out.println("Is alive: " + thread2.isAlive());
-        System.out.println("Get class: " + thread2.getClass());
-        System.out.println("---------------------------");
+        daemonThreadInfo(thread2);
 
     }
 }
